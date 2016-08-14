35041879591a528d8a24801c8b1fce320321ccb6
Frédéric Dupérier <fduperier@gmail.com>
2016-08-14 19:57:42 +0200

    ajout du module http

--- app/border-component.ts
+++ app/border-component.ts
@@ -2,8 +2,20 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'my-fancy-border',
-  templateUrl: 'app/border-component.html',
-  styleUrls: ['app/border-component.css'],
+  template: `<div class="outer">
+    <span>-- {{title}} --</span>
+    <div class="inner">
+        <ng-content></ng-content>
+        </div>
+        <span>-- {{title}} --</span>
+    </div>
+    `,
+  styles: [`
+    div {border: 5px solid #EDF3F3; border-radius: 1em; text-align: center}
+    span {font-size: 10px;display: inline-block}
+    .inner {padding: 5px; margin: 5px}
+    .outer {width: 400px}
+  `],
   inputs: ['title: title']
 })
 export class BorderComponent {
