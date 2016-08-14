f9408955afb47a44967cfc1845fe8ece632e3214
Frédéric Dupérier <fduperier@gmail.com>
2016-08-14 22:31:15 +0200

    revert sur suppression html et css

--- app/border-component.ts
+++ app/border-component.ts
@@ -2,20 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'my-fancy-border',
-  template: `<div class="outer">
-    <span>-- {{title}} --</span>
-    <div class="inner">
-        <ng-content></ng-content>
-        </div>
-        <span>-- {{title}} --</span>
-    </div>
-    `,
-  styles: [`
-    div {border: 5px solid #EDF3F3; border-radius: 1em; text-align: center}
-    span {font-size: 10px;display: inline-block}
-    .inner {padding: 5px; margin: 5px}
-    .outer {width: 400px}
-  `],
+  templateUrl: '/app/border-component.html',
+  styleUrls: ['/app/border-component.css'],
   inputs: ['title: title']
 })
 export class BorderComponent {
